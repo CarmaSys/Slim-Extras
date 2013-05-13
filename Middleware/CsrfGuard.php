39145b45d617561486557ea6920fcd5566379324
Christopher Najewicz <cnajewicz@carmasys.com>
2013-05-13 10:24:34 -0400

    CSRF cookie support for JS libraries that use it like AngularJS.

--- Middleware/CsrfGuard.php
+++ Middleware/CsrfGuard.php
@@ -23,18 +23,27 @@ class CsrfGuard extends \Slim\Middleware
     protected $key;
 
     /**
+     * Use cookie for CSRF token.
+     *
+     * @var bool
+     */
+    protected $useCookie = false;
+
+    /**
      * Constructor.
      *
      * @param string    $key        The CSRF token key name.
+     * @param bool      $cookie     Use a cookie to store CSRF token.
      * @return void
      */
-    public function __construct($key = 'csrf_token')
+    public function __construct($key = 'csrf_token', $cookie = false)
     {
         if (! is_string($key) || empty($key) || preg_match('/[^a-zA-Z0-9\-\_]/', $key)) {
             throw new \OutOfBoundsException('Invalid CSRF token key "' . $key . '"');
         }
 
         $this->key = $key;
+        $this->useCookie = $cookie;
     }
 
     /**
@@ -52,6 +61,39 @@ class CsrfGuard extends \Slim\Middleware
     }
 
     /**
+     * Generate CSRF Token.
+     *
+     * @return string
+     */
+    private static function generateToken() {
+        return sha1(serialize($_SERVER) . rand(0, 0xffffffff));
+    }
+
+    /**
+     * Get the current CSRF token according to session or cookie storage.
+     *
+     * @return string|false
+     */
+    protected function getToken() {
+        if($this->useCookie && $this->app->getCookie($this->key)) {
+            return $this->app->getCookie($this->key);            
+        } else if(!$this->useCookie && isset($_SESSION[$this->key])) {
+            return $_SESSION[$this->key];
+        }
+        return false;
+    }
+
+    /**
+     * Set the current CSRF token according to session or cookie storage.
+     * @param string    $token      CSRF token.
+     * @return void
+     */
+    protected function setToken($token) {
+        $this->useCookie ? $this->app->setCookie($this->key,$token) : $_SESSION[$this->key] = $token;
+    }
+
+
+    /**
      * Check CSRF token is valid.
      * Note: Also checks POST data to see if a Moneris RVAR CSRF token exists.
      *
@@ -59,19 +101,25 @@ class CsrfGuard extends \Slim\Middleware
      */
     public function check() {
         // Check sessions are enabled.
-        if (session_id() === '') {
+        if (!$this->useCookie && session_id() === '') {
             throw new \Exception('Sessions are required to use the CSRF Guard middleware.');
         }
 
-        if (! isset($_SESSION[$this->key])) {
-            $_SESSION[$this->key] = sha1(serialize($_SERVER) . rand(0, 0xffffffff));
+        if(!$this->getToken()) {
+            $token = self::generateToken();
+            $this->setToken($token);
+        } else {
+            $token = $this->getToken();    
         }
-
-        $token = $_SESSION[$this->key];
 
         // Validate the CSRF token.
         if (in_array($this->app->request()->getMethod(), array('POST', 'PUT', 'DELETE'))) {
-            $userToken = $this->app->request()->post($this->key);
+            // Check headers for X-XSRF-TOKEN to support AngularJS
+            if($this->app->request()->isAjax() && $this->app->request()->headers('X-'.$this->key)) {
+                $userToken = $this->app->request()->headers('X-' . $this->key);
+            } else {
+                $userToken = $this->app->request()->post($this->key);
+            }
             if ($token !== $userToken) {
                 $this->app->halt(400, 'Invalid or missing CSRF token.');
             }
